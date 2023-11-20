352c2ddaa7f4514b7e57cfbb568df50d3c5f2a63
Gabriele oliveira <131557956+GbNymos@users.noreply.github.com>
2023-11-20 18:52:22 -0300

    tarefas
    versao1 do organizador de tarefas.

---
+++ index.js
@@ -0,0 +1,58 @@
+
+
+function adicionarTarefa() {
+    // pegar o valor q esta no campo
+    var adiciona = document.getElementById('add').value;
+
+    // ver se o campo esta vazio
+    if (adiciona.trim() !== '') {
+
+        // Cria novo elemento 
+        var novaTarefaElemento = document.createElement('li');
+
+        // faz o texto aparecer no conteiner
+        novaTarefaElemento.textContent = adiciona;
+
+        // Adicionar a nova tarefa à lista
+        document.getElementById('listas').appendChild(novaTarefaElemento);
+    
+        //botao excluir e concluir
+        var btn_excluir =document.createElement("button");
+        btn_excluir.textContent = "excluir";
+        btn_excluir.onclick=function(){
+            excluir(novaTarefaElemento)
+        };
+        novaTarefaElemento.appendChild(btn_excluir);
+
+        var btn_concluir=document.createElement("button");
+        btn_concluir.textContent="concluir";
+        btn_concluir.onclick=function(){
+            concluir(novaTarefaElemento)
+        };
+        novaTarefaElemento.appendChild(btn_concluir);
+
+
+        // Limpar o campo input
+        document.getElementById('add').value = '';
+    } else {
+        // Alerta se o campo estiver vazio
+        alert('Por favor, digite uma nova tarefa.');
+    }
+}
+
+
+
+function excluir(tarefa){
+    tarefa.remove();
+}
+
+
+function concluir(tarefa){
+    tarefa.style.color="darkgreen";
+    tarefa.style.textDecoration = "line-through";
+  
+}
+
+
+
+     
